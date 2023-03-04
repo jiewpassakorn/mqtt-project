411dff652006c5998af5a55181b1a68c068045d0
Passakorn Klaikaew <passakornjiew@gmail.com>
2023-03-05 05:28:47 +0700

    add comment

--- pub.py
+++ pub.py
@@ -8,19 +8,26 @@ from paho.mqtt import client as mqtt_client
 import socket
 from decouple import config
 
-
+# Get IP address of current machine
 IP_ADDRESS = socket.gethostbyname(socket.gethostname())
+
+# Define maximum packet size for MQTT messages
 MAX_PACKET_SIZE = 250
+
+# Define MQTT broker settings
 BROKER = "broker.emqx.io"
 PORT = 1883
 TOPIC = "python/mqtt-ohm"
-# generate client ID with pub prefix randomly
+
+# Generate client ID with pub prefix randomly
 CLIENT_ID = f"python-mqtt-{random.randint(0, 1000)}"
+
+# Set MQTT username and password
 USERNAME = "emqx"
 PASSWORD = "public"
 
-
 def connect_mqtt():
+    # Define MQTT connection handler
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
@@ -28,14 +35,23 @@ def connect_mqtt():
         else:
             print("Failed to connect, return code %d\n", rc)
 
+    # Create new MQTT client instance
     client = mqtt_client.Client(CLIENT_ID)
+    
+    # Set MQTT username and password
     client.username_pw_set(USERNAME, PASSWORD)
+    
+    # Set MQTT connection handler
     client.on_connect = on_connect
+    
+    # Connect to MQTT broker
     client.connect(BROKER, PORT)
+    
     return client
 
 
 def on_disconnect(client, userdata, rc):
+    # Define handler for MQTT disconnection
     if rc != 0:
         print("unexpected to disconnect, return code %d\n", rc)
     else:
@@ -43,21 +59,30 @@ def on_disconnect(client, userdata, rc):
 
 
 def read_sensor_data():
-    # Read sensor data from Excel file
+    # Read sensor data from Excel file using pandas library
     df = pd.read_excel("SampleInput.xlsx")
     return df.to_dict("records")
 
 
 def publish(client):
+    # Get sensor data as a list of dictionaries
     sensor_data = read_sensor_data()
+    
+    # Loop through sensor data and publish to MQTT broker
     for i, data in enumerate(sensor_data):
+        # Generate unique ID for data packet
         data_id = str(uuid.uuid4())
         time.sleep(0.5)
+        
+        # Convert data dictionary to string
         msg = str(data)
+        
+        # Split message into multiple packets if larger than maximum packet size
         packets = [
             msg[i: i + MAX_PACKET_SIZE] for i in range(0, len(msg), MAX_PACKET_SIZE)
         ]
 
+        # Publish packets to MQTT broker
         for j, packet in enumerate(packets):
             time.sleep(0.3)
             result = client.publish(
@@ -68,20 +93,24 @@ def publish(client):
             else:
                 print(f"Failed to send message to topic {TOPIC}")
 
+        # Publish end-of-packet marker to MQTT broker
         time.sleep(0.5)
         client.publish(TOPIC, f"{IP_ADDRESS}, {data_id}, -1, end")
 
+    # Publish end-of-data marker to MQTT broker
     time.sleep(0.5)
     client.publish(TOPIC, ",,,enddEIEI")
     print("endEIEI")
 
 
 def run():
+    # Connect to MQTT broker and publish sensor data
     client = connect_mqtt()
     publish(client)
+    
+    # Set MQTT disconnection handler and disconnect from broker
     client.on_disconnect = on_disconnect
     client.disconnect()
-
 
 if __name__ == "__main__":
 
--- sub.py
+++ sub.py
@@ -1,27 +1,33 @@
-import random
-import mysql.connector
-import paho.mqtt.client as mqtt
+# Import necessary libraries
+import random  # For generating random numbers
+import mysql.connector  # For connecting to MySQL database
+import paho.mqtt.client as mqtt  # For MQTT communication
+import os  # For loading environment variables
+from dotenv import load_dotenv  # For loading environment variables from .env file
 
-
-import os
-from dotenv import load_dotenv
-
+# Load environment variables from .env file
 load_dotenv()
 USER = os.getenv("USER")
 DB_NAME = os.getenv("DB_NAME")
 PASSWORD = os.getenv("PASSWORD")
 TABLE_NAME = os.getenv("TABLE_NAME")
 
+# MQTT Broker configuration
 broker = "broker.emqx.io"
 port = 1883
 topic = "python/mqtt-ohm"
 
-# Generate client ID with pub prefix randomly
+# Generate a random client ID with "python-mqtt-" prefix
 client_id = f"python-mqtt-{random.randint(0, 100)}"
+
+# MQTT Broker credentials
 username = "emqx"
 password = "public"
 
+# Create a dictionary to store messages received from each unique device ID
 global_dict = {}
+
+# MQTT client callback function for when it successfully connects to the MQTT Broker
 
 
 def on_connect(client, userdata, flags, rc):
@@ -31,52 +37,86 @@ def on_connect(client, userdata, flags, rc):
     else:
         print(f"Failed to connect, return code {rc}\n")
 
+# MQTT client callback function for when it receives a message from a subscribed topic
+
 
 def on_message(client, userdata, msg):
+    # Decode message payload
     payload = msg.payload.decode()
+
+    # Split payload into IP address, UID, index, and message parts
     ip_address, uid, index, message = payload.split(",", 3)
+
+    # Strip whitespace from message part
     message = message.strip()
 
+    # Add message to dictionary for that UID
     add_value(ip_address.strip(), uid, index, message)
+
+# Function to connect to MQTT Broker
 
 
 def connect_mqtt() -> mqtt.Client:
+    # Create MQTT client object
     client = mqtt.Client(client_id)
+
+    # Set MQTT client username and password
     client.username_pw_set(username, password)
+
+    # Set MQTT client callback function for when it successfully connects to the MQTT Broker
     client.on_connect = on_connect
+
+    # Connect to MQTT Broker
     client.connect(broker, port)
+
     return client
+
+# Function to subscribe to a topic on the MQTT Broker
 
 
 def subscribe(client: mqtt.Client):
+    # Subscribe to the topic
     client.subscribe(topic)
+
+    # Set MQTT client callback function for when it receives a message from a subscribed topic
     client.on_message = on_message
+
+# Function to add a message to the dictionary for that UID
 
 
 def add_value(ip_address, uid, index, message):
+    # If message is "end", split and insert the entire message for that UID into the database
     if message == "end":
         split_and_insert(ip_address, uid, global_dict[uid])
         del global_dict[uid]
     else:
+        # If the UID already exists in the dictionary, concatenate the new message to the existing message for that UID
         if uid in global_dict.keys():
             data = global_dict[uid] + message
             global_dict.update({uid: data})
+        # If the UID does not exist in the dictionary, add the new message to the dictionary for that UID
         else:
             global_dict.update({uid: message})
 
+# This function splits the incoming message into timestamp, temperature, humidity, and thermalarray parts and calls the insert_to_database function to store the data in the database.
+
 
 def split_and_insert(ip_address, uid, message):
+    # Split message into timestamp, temperature, humidity, and thermalarray parts
     data = message.split(",", 3)
     timestamp = data[0].split(":", 1)[1].strip().split("'", 2)[1]
     temperature = float(data[1].split(":", 1)[1].strip())
     humidity = float(data[2].split(":", 1)[1].strip())
     thermalarray = data[3].split(":", 1)[1].strip()[1:-2]
+    # Call insert_to_database function to store data in database
     insert_to_database(ip_address, uid, timestamp,
                        temperature, humidity, thermalarray)
 
 
+# This function connects to the MySQL database and inserts the provided data into the table.
 def insert_to_database(ip_address, uid, timestamp, temperature, humidity, thermalarray):
     try:
+        # Connect to MySQL database
         connection = mysql.connector.connect(
             host="localhost",
             user=USER,
@@ -85,18 +125,23 @@ def insert_to_database(ip_address, uid, timestamp, temperature, humidity, therma
         )
 
         with connection.cursor() as cursor:
+            # SQL statement to insert data into table
             sql = f"INSERT INTO `{TABLE_NAME}` (ip_address, time, humidity, temperature, thermal_array) VALUES (%s, %s, %s, %s, %s)"
             values = (ip_address, timestamp, humidity,
                       temperature, thermalarray)
+            # Execute the SQL statement
             cursor.execute(sql, values)
 
+        # Commit the changes
         connection.commit()
+        # Print message indicating the insert was completed
         print("Insert completed", uid, ip_address)
     except Exception as e:
+        # Print error message if there was an exception
         print(f"Failed to write to MySQL database: {e}")
     finally:
+        # Close the connection to the database
         connection.close()
-
 
 def run():
     client = connect_mqtt()
